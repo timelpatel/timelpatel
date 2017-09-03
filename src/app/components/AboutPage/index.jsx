a9469da0c229696c8ef6a74ff36f589b8c2f8d04
Timel Patel <username@users.noreply.github.com>
2017-09-03 15:39:54 +0100

    Updated About section. Modified global styles.

--- src/app/components/AboutPage/index.jsx
+++ src/app/components/AboutPage/index.jsx
@@ -4,7 +4,11 @@ import React from 'react'
 const AboutPage = () => (
     <div className='container__about'>
         <div className='me' />
-        <h3>Based in London, Timel provides creative and development services to leading organisations and technology start-ups.</h3>
+
+        <div className='wrapper'>
+            <h2>Hello.</h2>
+            <h3>London based software engineer, Timel provides creative and development services to leading organisations and technology start-ups.</h3>
+        </div>
     </div>
 )
 
--- src/app/components/Header/index.jsx
+++ src/app/components/Header/index.jsx
@@ -5,7 +5,7 @@ const Header = () => (
     <header className='container__header'>
         <h1 className='header__h1'>Timel Patel</h1>
         <ul className='header__menu'>
-            <li><a href='#'>About</a></li>
+            <li><a href='/'>About</a></li>
             <li><a href='#'>Work</a></li>
         </ul>
     </header>
